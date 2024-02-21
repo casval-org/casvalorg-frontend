1c34381fe9fa4c3e250db56484f63a48c2d2c454
R.Berk Karatas <rberkkaratas@gmail.com>
2024-02-21 16:51:29 +0300

    update: data & banner updated

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Banner() {
+
+    const [email, setEmail] = useState('');
 
     if (localStorage.getItem("subscribed") === "true") {
         return null
@@ -15,14 +17,28 @@ export default function Banner() {
         event.preventDefault();
         localStorage.setItem("subscribed", true);
         closeBanner(event);
-    }
 
+        fetch('https://ecommerce-api-ten-sooty.vercel.app/api/v1/subscribe', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    }
     return (
         <div id="newsletter-banner" tabindex="-1" class="fixed bottom-0 end-0 z-50 flex justify-between w-full p-4 border-b border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
             <div class="flex items-center flex-shrink-0 w-full mx-auto sm:w-auto">
                 <form class="flex flex-col items-center w-full md:flex-row" onSubmit={subscribe}>
                     <label for="email" class="flex-shrink-0 mb-2 me-auto text-sm font-medium text-gray-500 md:mb-0 md:me-4 dark:text-gray-400 md:m-0">CASVAL Etkinlik ve Duyuru Bülteni'ne abone ol</label>
-                    <input type="email" id="email" placeholder="Eposta adresi giriniz" class="bg-white border border-gray-300 text-gray-900 md:w-64 mb-2 md:mb-0 md:me-4 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                    <input type="email" id="email" placeholder="Eposta adresi giriniz" class="bg-white border border-gray-300 text-gray-900 md:w-64 mb-2 md:mb-0 md:me-4 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required  onChange={e => setEmail(e.target.value)}/>
                     <button type="submit" class="text-white bg-green-500 hover:bg-green-500 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-400 dark:hover:bg-green-400 dark:focus:ring-green-500">Abone Ol</button>
                 </form>
             </div>
